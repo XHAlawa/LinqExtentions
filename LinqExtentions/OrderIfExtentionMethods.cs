e424be1027215606857da5db2a18f3721e7af19f
AhmedHalawa <ahmed.halawa@curenta.com>
2023-01-29 10:34:45 +0200

    Inital Of Extentions

---
+++ LinqExtentions/OrderIfExtentionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExtentions
+{
+    public static class OrderIfExtentionMethods
+    {
+        public static IOrderedQueryable<TSource> OrderByIf<TSource, TKey>(this IQueryable<TSource> query, bool condition, Expression<Func<TSource, TKey>> orderByExpression)
+        =>  condition ? query.OrderBy(orderByExpression) : (IOrderedQueryable<TSource>)query;
+
+        public static IOrderedQueryable<TSource> OrderByDescendingIf<TSource, TKey>(this IQueryable<TSource> query, bool condition, Expression<Func<TSource, TKey>> orderByExpression)
+        =>  condition ? query.OrderByDescending(orderByExpression) : (IOrderedQueryable<TSource>)query;
+
+        public static IOrderedQueryable<TSource> ThenByIf<TSource, TKey>(this IOrderedQueryable<TSource> query, bool condition, Expression<Func<TSource, TKey>> orderByExpression)
+        =>  condition ? query.ThenBy(orderByExpression) : query;
+
+        public static IOrderedQueryable<TSource> ThenByDescendingIf<TSource, TKey>(this IOrderedQueryable<TSource> query, bool condition, Expression<Func<TSource, TKey>> orderByExpression)
+        => condition ? query.ThenByDescending(orderByExpression) : query;
+    }
+}
---
+++ LinqExtentions/PageingExtentionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExtentions
+{
+    public static class PageingExtentionMethods
+    {
+        public static (List<T> Items,int Total) ToPagedList<T>(this IQueryable<T> query, int page = 0, int countPerPage = 10)
+        {
+            var pagedItems = query.Skip(page * countPerPage).Take(countPerPage).ToList();
+            return (pagedItems, query.Count());
+        } 
+    }
+}
